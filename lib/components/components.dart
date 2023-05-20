631f8ef3cba544cea647e1efc9546ab0d4862c0a
Eldar2021 <eldiiaralmazbekov@gmail.com>
2023-05-20 23:42:30 +0600

    home ui

---
+++ lib/components/components.dart
@@ -0,0 +1,3 @@
+export 'views/error_widget.dart';
+export 'views/initial_widget.dart';
+export 'views/loading_widget.dart';
--- lib/modules/home/view/home_view.dart
+++ lib/modules/home/view/home_view.dart
@@ -1,4 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_svg/svg.dart';
+
+import 'package:learn_world/components/components.dart';
+import 'package:learn_world/core/core.dart';
+import 'package:learn_world/models/models.dart';
+import 'package:learn_world/modules/modules.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
@@ -6,8 +13,72 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('HomeView'),
+      body: BlocBuilder<HomeCubit, HomeState>(
+        builder: (context, state) {
+          switch (state.fetchStatus) {
+            case FetchStatus.initial:
+              return const InitialWidget();
+            case FetchStatus.loading:
+              return const LoadingWidget();
+            case FetchStatus.success:
+              return HomeSuccesView(state.contents ?? []);
+            case FetchStatus.fail:
+              return const CustomErrorWidget();
+          }
+        },
+      ),
+    );
+  }
+}
+
+class HomeSuccesView extends StatelessWidget {
+  const HomeSuccesView(this.contents, {super.key});
+
+  final List<Content> contents;
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      initialIndex: 0,
+      length: contents.length,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('HomeView'),
+          bottom: TabBar(
+            isScrollable: true,
+            tabs: contents
+                .map((e) => Tab(
+                      child: Row(
+                        children: [
+                          if (e.svgUrl != null) SvgPicture.network(e.svgUrl!, width: 35, height: 35),
+                          const SizedBox(width: 10),
+                          Text(e.name),
+                        ],
+                      ),
+                    ))
+                .toList(),
+          ),
+        ),
+        body: TabBarView(
+          children: contents
+              .map(
+                (e) => ListView.builder(
+                  padding: const EdgeInsets.fromLTRB(14, 25, 14, 40),
+                  itemCount: e.articles.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    final article = e.articles[index];
+                    return Card(
+                      child: ListTile(
+                        leading: Text(article.id),
+                        title: Text(article.name),
+                        trailing: Text(article.locale),
+                      ),
+                    );
+                  },
+                ),
+              )
+              .toList(),
+        ),
       ),
     );
   }
