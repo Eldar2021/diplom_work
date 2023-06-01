000d150e85f876842923eb844d017c3e201d2c8b
Eldar2021 <eldiiaralmazbekov@gmail.com>
2023-06-01 23:39:57 +0600

    added api dart app

---
+++ api/bin/api.dart
@@ -0,0 +1,8 @@
+import 'package:api/api.dart';
+
+void main() async {
+  // await createPaste('003');
+  // await getPaste(id: 'UuHHv', fileName: 'test');
+  // await updatePaste(id: 'AaroC', fileName: '002');
+  await deletePaste(id: 'UuHHv');
+}
