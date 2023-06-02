c906eaf9c06510ae01c13a25f8aee67f09cdd418
Eldar2021 <eldiiaralmazbekov@gmail.com>
2023-06-03 01:51:31 +0600

    success localizations

--- lib/constants/api/api_const.dart
+++ lib/constants/api/api_const.dart
@@ -2,4 +2,5 @@ class ApiConst {
   static const eldarApiBase = 'https://eldar2021.github.io';
   static const main = '$eldarApiBase/learn_world/data/main.json';
   static const urlGitHub = 'https://github.com/Eldar2021/learn_world';
+  static const rocketBase = 'https://paste.rs';
 }
