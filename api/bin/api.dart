33c6853b6f96b4d92ed74c73de55ca27afc1f608
Eldar2021 <eldiiaralmazbekov@gmail.com>
2023-06-03 21:27:28 +0600

    added 004 full

--- api/bin/api.dart
+++ api/bin/api.dart
@@ -1,8 +1,8 @@
 import 'package:api/api.dart';
 
 void main() async {
-  final filePath = './data/tr/003';
-  for (var i = 1; i < 3; i++) {
+  final filePath = './data/tr/004';
+  for (var i = 1; i < 5; i++) {
     await createPaste(
       uploadFile: '$filePath/$i.md',
       downloadFile: '$filePath/links.md',
