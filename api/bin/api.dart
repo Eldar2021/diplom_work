9498bfc78de70f0bffe8f13a52a5a23310d5a471
Eldar2021 <eldiiaralmazbekov@gmail.com>
2024-05-20 22:04:02 +0600

    update topics

--- api/bin/api.dart
+++ api/bin/api.dart
@@ -1,8 +1,8 @@
 import 'package:api/api.dart';
 
 void main() async {
-  final filePath = './data/tr/014';
-  for (var i = 1; i < 6; i++) {
+  final filePath = './data/_topics';
+  for (var i = 1; i < 3; i++) {
     await createPaste(
       uploadFile: '$filePath/$i.md',
       downloadFile: '$filePath/links.md',
--- api/data/_topics/links.md
+++ api/data/_topics/links.md
@@ -1,3 +1,2 @@
-
-./data/_topics/1.md-https://paste.rs/DYmrJ
-./data/_topics/2.md-https://paste.rs/qnOWy
+./data/_topics/1.md-https://paste.rs/jzLxy
+./data/_topics/2.md-https://paste.rs/MXT8b
